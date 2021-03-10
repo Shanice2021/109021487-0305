d0d6ef13142e7a7ba843e4b535dbf4a1032913dd
Shanice2021 <109021487@live.asia.edu.tw>
2021-03-10 23:12:57 +0800

    pythonHW

---
+++ runMM2.py
@@ -0,0 +1,17 @@
+import sys
+import mymodule as mm
+
+# 最大公因數與最小公倍數
+a=int(sys.argv[1]) #M2Q9
+b=int(sys.argv[2]) #M2Q9
+print(mm.M2Q9(a,b))
+
+# 數字矩陣
+a=int(sys.argv[1]) #M3Q4
+b=int(sys.argv[2]) #M3Q4
+print(mm.M3Q4(a,b))
+
+#C(m,n)
+a=int(sys.argv[1]) #M3Q5
+b=int(sys.argv[2]) #M3Q5
+print(mm.M3Q5(a,b))
